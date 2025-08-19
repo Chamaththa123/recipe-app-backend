c12737f33559a997d1bf149d941a73e68e8c99ed
Chamaththa123 <shamodchamaththa@gmail.com>
2025-08-19 23:53:39 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   full_name: {
@@ -16,4 +16,4 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-export default mongoose.model("user", userSchema);
+module.exports = mongoose.model("user", userSchema);
