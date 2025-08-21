9d6a10d6f238d9e2ee9a269aea68f90841e4d2bb
Chamaththa123 <shamodchamaththa@gmail.com>
2025-08-21 13:55:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/recipes", recipeRoutes);
 app.use("/api/favorites", favoriteRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
