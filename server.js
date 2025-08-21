1a8f1e343b36da119793d2f7d350e210965fbe0a
Chamaththa123 <shamodchamaththa@gmail.com>
2025-08-21 16:04:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000", // Only allow requests from http://localhost:3000
+    origin: "https://recipe-app-backend-production-3ccf.up.railway.app", // Only allow requests from http://localhost:3000
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
